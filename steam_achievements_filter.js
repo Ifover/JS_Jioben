57cc12c01c980dfb1bbb8867d015fa14a05eff0b
Ifover <1224842990@qq.com>
2022-01-24 15:15:02 +0800

    Update steam_achievements_filter.js
    0.2 记录上一次选择的值

--- steam_achievements_filter.js
+++ steam_achievements_filter.js
@@ -1,15 +1,18 @@
 // ==UserScript==
 // @name         steam成就过滤
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  过滤steam成就，分类已完成和未完成
+// @namespace    https://github.com/Ifover/JS_jioben
+// @version      0.2
+// @description  过滤steam成就，已完成和未完成
 // @author       Ifover
 // @match        *://steamcommunity.com/stats/*/achievements/
 // @icon         https://steamcommunity.com/favicon.ico
-// @grant        none
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @license      MIT
 // ==/UserScript==
 
 var achievementsStatus;
+var id
 
 function _init() {
     var append_style = `
@@ -18,6 +21,45 @@ function _init() {
       </style>
     `
     jQuery('head').append(append_style);
+
+
+
+    let headerContentLeft = jQuery('#headerContentLeft')
+    let action = jQuery(`
+      <div>
+          成就状态: 
+          <a class="achievements-txt all" style="color: #fff">全部</a>
+           /         
+          <a class="achievements-txt unlocked">已完成</a>
+           / 
+          <a class="achievements-txt locked">未完成</a>
+      </div>
+    `)
+    headerContentLeft.append(action)
+
+
+    jQuery('.achievements-txt').on('click', function () {
+        handleToggle(jQuery(this).index());
+        GM_setValue('achievements' + id, jQuery(this).index())
+
+        jQuery('.achievements-txt').css({color: "#898989"})
+        jQuery(this).css({color: "#fff"})
+    })
+
+
+    let match = location.pathname.match(/\d+/g)
+    id = match[0]
+
+    if (GM_getValue('achievements' + id)) {
+        achievementsStatus = GM_getValue('achievements' + id)
+        jQuery('.achievements-txt').css({color: "#898989"})
+        jQuery(jQuery('.achievements-txt')[achievementsStatus]).css({color: "#fff"})
+    } else {
+        GM_setValue('achievements' + id, 0)
+    }
+
+    handleToggle()
+
 }
 
 function handleToggle(aStatus = achievementsStatus) {
@@ -43,24 +85,6 @@ function handleToggle(aStatus = achievementsStatus) {
 (function () {
     'use strict';
     _init()
-    achievementsStatus = 0
-    let headerContentLeft = jQuery('#headerContentLeft')
-    let action = jQuery(`
-      <div style="color: #898989">
-          成就状态: 
-          <a class="achievements-txt all" style="color: #fff">全部</a>
-           /         
-          <a class="achievements-txt unlocked">已完成</a>
-           / 
-          <a class="achievements-txt locked">未完成</a>
-      </div>
-    `)
-    headerContentLeft.append(action)
-    jQuery('.achievements-txt').on('click', function () {
-        handleToggle(jQuery(this).index());
-        jQuery('.achievements-txt').css({color: "#898989"})
-        jQuery(this).css({color: "#fff"})
-    })
 
 
 })();
